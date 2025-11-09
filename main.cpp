61153bb29c09b82768baaf53fb43134487aa6a1f
Marvin <marvin.boo@student.mahidol.edu>
2025-11-09 07:31:00 +0000

    updated

--- main.cpp
+++ main.cpp
@@ -4,52 +4,49 @@
 
 using namespace std;
 
-int main(int argc,char *argv[]){
-int *pa, *pb, i, temp;
-int n;
-
-cout<<"How many numbers? ";
-cin>>n;
-pa=new int[n];
-pb = pa;
-
-for (i=0;i<n;i++)
+int main(int argc,char *argv[])
 {
-  cout<<"Input no."<<i+1<<": ";
-  cin>>*pa;
-  pa++;
-}
+   int *pa, *pb, i, temp;
+   int n;
 
-pa-=n;
+   n = argc - 1;  // The number of elements is the number of arguments minus 1
 
-cout<<"Original: ";
-for (i=0; i<n-1;i++, pa++)
-{
-   cout<<setw(3)<<*pa;//<<endl;
-}
-cout<<setw(3)<<*pa<<endl;
+   pa = new int[n];
+   pb = pa;
 
-pb=pa;
-pa-=(n-1);
+   for (i = 0; i < n; i++) 
+    {
+        pa[i] = atoi(argv[i + 1]);  // Convert each argument to an integer and store it
+    }
 
-for (i=0; i<n/2; i++)
-{
-   temp = *pa;  
-   *pa = *pb;  
-   *pb = temp;
-   pa++;  pb--;
-}
-pa-=(n/2);
+      cout<<"Original: ";
+      for (i=0; i<n-1;i++, pa++)
+      {
+         cout<<setw(3)<<*pa;//<<endl;
+      }
+      cout<<setw(3)<<*pa<<endl;
+
+      pb=pa;
+      pa-=(n-1);
+
+      for (i=0; i<n/2; i++)
+      {
+         temp = *pa;  
+         *pa = *pb;  
+         *pb = temp;
+         pa++;  pb--;
+      }
+      pa-=(n/2);
 
 
-cout<<"Reversed: ";
-for (i=0; i<n-1; i++){
-   cout<<setw(3)<<*pa ;//<<endl;
-  pa++;
-}
-cout<<setw(3)<<*pa<<endl;
-pa-=(n-1);
-delete []pa;
+      cout<<"Reversed: ";
+      for (i=0; i<n-1; i++){
+         cout<<setw(3)<<*pa ;//<<endl;
+      pa++;
+      }
+      cout<<setw(3)<<*pa<<endl;
+      pa-=(n-1);
+      delete []pa;
 
-return 0;
+      return 0;
 }
